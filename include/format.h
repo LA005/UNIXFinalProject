721d62c0da3ace9fc7acd7cc00b5daf12b1ee8e8
LA005 <levi.m.anderson@okstate.edu>
2026-05-06 15:13:40 -0500

    Update format.h

--- include/format.h
+++ include/format.h
@@ -4,9 +4,9 @@
 #include <string>
 
 namespace Format {
-std::string Format(int);
-std::string ElapsedTime(long times);
-std::string KBisMB(float kb);
+std::string Format(int);//converts int into string
+std::string ElapsedTime(long times);//converts time 
+std::string KBisMB(float kb);//converts size from KB to MB
 };  // namespace Format
 
 #endif
