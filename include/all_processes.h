c1cbab4dc90065d49fbc098028f349b9e0e6993d
LA005 <levi.m.anderson@okstate.edu>
2026-05-06 15:12:17 -0500

    Update all_processes.h

--- include/all_processes.h
+++ include/all_processes.h
@@ -1,28 +1,25 @@
 #ifndef ALL_PROCESSES_H
 #define ALL_PROCESSES_H
-
 #include "process.h"
-
 #include <string>
 #include <vector>
-
 using std::string;
 using std::vector;
 
 class All_Processes {
  private:
-  long Hertz;
+  long Hertz; //sys tick rate
   vector<int> current_pids_;
   vector<Process> all_processes_;
 
   void UpdateProcesses();
-  void AddNewProcesses(bool&);
-  vector<int> ReadFolders();
-  void RemoveFinishedProcesses(bool&);
+  void AddNewProcesses(bool&);//detects new processes
+  vector<int> ReadFolders();//reads process directories
+  void RemoveFinishedProcesses(bool&);//removes processes when completed
 
  public:
-  All_Processes();
-  vector<Process>& GetProcesses();
+  All_Processes();//allows tracking structures and system timing
+  vector<Process>& GetProcesses();//returns vectors of tracket processes
 };
 
 #endif
