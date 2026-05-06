fe09f02a585fb6ce4772dbe0d695c829bd19b3d9
LA005 <levi.m.anderson@okstate.edu>
2026-05-06 15:15:46 -0500

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -1,25 +1,22 @@
 #ifndef SYSTEM_PARSER_H
 #define SYSTEM_PARSER_H
-
 #include <fstream>
 #include <regex>
 #include <string>
-
 #include "parser_helper.h"
-
 using std::string;
 using std::vector;
 
 namespace LinuxParser {
 // System
-float MemoryUtilization();
-long UpTime();
-vector<int> Pids();
-int TotalProcesses();
-int RunningProcesses();
-string OperatingSystem();
-string Kernel();
-string UserByUID(int);
+float MemoryUtilization();//retrieves memory usage
+long UpTime();//retrieves system uptime
+vector<int> Pids();//retrieves all active processes
+int TotalProcesses();//retrieves number of processes
+int RunningProcesses();//retrieves number of running processes
+string OperatingSystem();//retrieves operating system specs
+string Kernel();//retrieves linux kernel
+string UserByUID(int);//Finds username
 
 std::vector<string> CpuUtilization();
 };  // namespace LinuxParser
