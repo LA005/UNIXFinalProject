501e8d2bb30de0f34a477354304132f434263877
LA005 <levi.m.anderson@okstate.edu>
2026-05-06 15:17:48 -0500

    Update ncurses_display.h

--- include/ncurses_display.h
+++ include/ncurses_display.h
@@ -7,10 +7,10 @@
 #include "system.h"
 
 namespace NCursesDisplay {
-void Display(System& system, int n = 10);
-void DisplaySystem(System& system, WINDOW* window);
-void DisplayProcesses(std::vector<Process> processes, WINDOW* window, int n);
-std::string ProgressBar(float percent);
+void Display(System& system, int n = 10);//Draws display function
+void DisplaySystem(System& system, WINDOW* window);//Displays info cpu,memory,process count,etc
+void DisplayProcesses(std::vector<Process> processes, WINDOW* window, int n);//displays the running processes
+std::string ProgressBar(float percent);// Turns them into a readable string
 };  // namespace NCursesDisplay
 
 #endif
